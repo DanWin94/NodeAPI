ae5773ef7d73bf2850c27146b4a0484b141135d1
DanWin94 <dannynguyen49@yahoo.com>
2019-09-03 14:03:48 -0400

    added order and product routes

---
+++ API/routes/products.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/',(req, res, next)=>{
+    res.status(200).json({
+        message :'Handling GET request to /products'
+    });
+});
+
+router.post('/',(req, res, next)=>{
+    res.status(201).json({
+        message :'Handling POST request to /products'
+    });
+});
+
+router.get('/:productID',(req, res, next)=>{
+    const productID = req.params.productID;
+    if(productID==='021'){
+    res.status(200).json({
+        message :'You passed the correct date'
+    });
+    }
+    else{
+        res.status(200).json({
+            message:'You passed a date'
+        })
+    }
+});
+
+router.patch('/:productID',(req, res, next)=>{
+    res.status(200).json({
+        message :'Updating product'
+    });
+});
+
+router.delete('/:productID',(req, res, next)=>{
+    res.status(200).json({
+        message :'deleting product'
+    });
+});
+
+module.exports = router;
--- app.js
+++ app.js
@@ -1,7 +1,18 @@
 const express = require('express');
 const app =express();
-const calenderRoutes = require('./api/routes/calender');
+var bodyParser = require("body-parser");
 
-app.use('/calender', calenderRoutes);
+app.use(bodyParser.json()); 
+
+
+const productRoutes = require ('./api/routes/products');
+const orderRoutes = require ('./api/routes/orders');
+app.use('/products', productRoutes);
+app.use('/orders', orderRoutes);
+
+
+
+
+
 
 module.exports=app;
